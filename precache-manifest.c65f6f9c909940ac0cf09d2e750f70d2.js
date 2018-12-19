5e20dedaff7428adfe1467857d8e858d34c80b45
lixijun <lixijun@baidu.com>
2018-12-19 17:47:06 +0800

    update

---
+++ precache-manifest.c65f6f9c909940ac0cf09d2e750f70d2.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "b3c99b867fbc24578f43",
+    "url": "/js/chunk-vendors.fd7d129a.js"
+  },
+  {
+    "revision": "0330f281beb20935e62b",
+    "url": "/js/app.d5dab6a3.js"
+  },
+  {
+    "revision": "e122297800f853536138",
+    "url": "/js/about.896d1847.js"
+  },
+  {
+    "revision": "e9a5ae489dd4c153c96f1465bb708b8d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0a71a89ad9892b52182d0e09080fed2e",
+    "url": "/img/zodiac-mystery.0a71a89a.jpg"
+  },
+  {
+    "revision": "48278458f75fc233297bdab50908ac78",
+    "url": "/img/the-hivemind-mount.48278458.jpg"
+  },
+  {
+    "revision": "71501fafae9cdecdb684c90a69d4b4b3",
+    "url": "/img/jumping-puzzle.71501faf.jpg"
+  },
+  {
+    "revision": "ef7d3c2a83453526c0d83c0d934242b3",
+    "url": "/img/arcane-lava.ef7d3c2a.jpg"
+  },
+  {
+    "revision": "b3c99b867fbc24578f43",
+    "url": "/css/chunk-vendors.d4ec3c11.css"
+  }
+];
