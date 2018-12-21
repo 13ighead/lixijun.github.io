d43cac9ce192269fc3b532ab7996c521569031fd
lixijun <lixijun@baidu.com>
2018-12-21 17:05:05 +0800

    update

---
+++ hivemind-puzzle/precache-manifest.ff1cbd37df395360a18e903cc5884b14.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = [
+  {
+    "revision": "02a2f855b8e597eca8b1",
+    "url": "/hivemind-puzzle/js/chunk-b7821564.d7af29a0.js"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/hivemind-puzzle/robots.txt"
+  },
+  {
+    "revision": "86bfd12742791b313c5f",
+    "url": "/hivemind-puzzle/css/chunk-c70606f6.9d6d5480.css"
+  },
+  {
+    "revision": "e122297800f853536138",
+    "url": "/hivemind-puzzle/js/about.896d1847.js"
+  },
+  {
+    "revision": "121a3c890240cc2ced3d",
+    "url": "/hivemind-puzzle/js/chunk-vendors.1783c78a.js"
+  },
+  {
+    "revision": "d8cdc0bc8a1c59afa0db",
+    "url": "/hivemind-puzzle/js/chunk-2b3f0ff5.64964242.js"
+  },
+  {
+    "revision": "1789797923e29156a967",
+    "url": "/hivemind-puzzle/js/chunk-db51673c.84f152da.js"
+  },
+  {
+    "revision": "ff0ce950e92e9027d3cc",
+    "url": "/hivemind-puzzle/js/chunk-4bac308c.d773f7ab.js"
+  },
+  {
+    "revision": "86bfd12742791b313c5f",
+    "url": "/hivemind-puzzle/js/chunk-c70606f6.6903f9b4.js"
+  },
+  {
+    "revision": "1a3c5c6ca86a1da43e2e",
+    "url": "/hivemind-puzzle/js/chunk-664912ed.6772d32b.js"
+  },
+  {
+    "revision": "b3ec251dc7c378a4beb5",
+    "url": "/hivemind-puzzle/js/chunk-0c6d2cea.d1c6a63c.js"
+  },
+  {
+    "revision": "abd44df735469a37f7cb",
+    "url": "/hivemind-puzzle/js/chunk-78a56aee.17dae89e.js"
+  },
+  {
+    "revision": "186c2e2167f9f32e709b",
+    "url": "/hivemind-puzzle/js/app.0922512f.js"
+  },
+  {
+    "revision": "d8cdc0bc8a1c59afa0db",
+    "url": "/hivemind-puzzle/css/chunk-2b3f0ff5.0e433876.css"
+  },
+  {
+    "revision": "02a2f855b8e597eca8b1",
+    "url": "/hivemind-puzzle/css/chunk-b7821564.0e433876.css"
+  },
+  {
+    "revision": "7d8847a6d694332ecdf6f6df6e830990",
+    "url": "/hivemind-puzzle/index.html"
+  },
+  {
+    "revision": "1789797923e29156a967",
+    "url": "/hivemind-puzzle/css/chunk-db51673c.0e433876.css"
+  },
+  {
+    "revision": "ef7d3c2a83453526c0d83c0d934242b3",
+    "url": "/hivemind-puzzle/img/arcane-lava.ef7d3c2a.jpg"
+  },
+  {
+    "revision": "121a3c890240cc2ced3d",
+    "url": "/hivemind-puzzle/css/chunk-vendors.cbbd3891.css"
+  },
+  {
+    "revision": "71501fafae9cdecdb684c90a69d4b4b3",
+    "url": "/hivemind-puzzle/img/jumping-puzzle.71501faf.jpg"
+  },
+  {
+    "revision": "0a71a89ad9892b52182d0e09080fed2e",
+    "url": "/hivemind-puzzle/img/zodiac-mystery.0a71a89a.jpg"
+  },
+  {
+    "revision": "48278458f75fc233297bdab50908ac78",
+    "url": "/hivemind-puzzle/img/the-hivemind-mount.48278458.jpg"
+  },
+  {
+    "revision": "abd44df735469a37f7cb",
+    "url": "/hivemind-puzzle/css/chunk-78a56aee.bd02ed02.css"
+  },
+  {
+    "revision": "1a3c5c6ca86a1da43e2e",
+    "url": "/hivemind-puzzle/css/chunk-664912ed.0e433876.css"
+  },
+  {
+    "revision": "ff0ce950e92e9027d3cc",
+    "url": "/hivemind-puzzle/css/chunk-4bac308c.0e433876.css"
+  },
+  {
+    "revision": "b3ec251dc7c378a4beb5",
+    "url": "/hivemind-puzzle/css/chunk-0c6d2cea.fba93c0d.css"
+  }
+];
--- hivemind-puzzle/service-worker.js
+++ hivemind-puzzle/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/hivemind-puzzle/precache-manifest.ac7bfd51aec598de04607cd52d414eb5.js"
+  "/hivemind-puzzle/precache-manifest.ff1cbd37df395360a18e903cc5884b14.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "hivemind-puzzle"});
